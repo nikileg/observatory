504c0595786c51a850b0cb01139593122b17d2a2
nikileg <10009432+nikileg@users.noreply.github.com>
2019-07-19 18:17:01 +0300

    wip color interpolation

--- src/main/scala/observatory/visualization/interpolation/ColorInterpolationService.scala
+++ src/main/scala/observatory/visualization/interpolation/ColorInterpolationService.scala
@@ -2,13 +2,21 @@ package observatory.visualization.interpolation
 
 import observatory.{Color, Temperature}
 
-class ColorInterpolationService(linearInterpolationService: InterpolationService) {
+import scala.collection.immutable.TreeMap
 
+class ColorInterpolationService(linearInterpolationService: InterpolationService)
+                               (points: Iterable[(Temperature, Color)]) {
   import linearInterpolationService.linearFract
 
-  def linearInterpolation(point1: (Temperature, Color),
-                          point2: (Temperature, Color),
-                          coordinate: Temperature): Color = {
+  def interpolateColor(temperature: Temperature): Color = {
+    val floor = treeMap.to(temperature).last
+    val ceil = treeMap.to(temperature).head
+    linearInterpolation(floor, ceil, temperature)
+  }
+
+  private[interpolation] def linearInterpolation(point1: (Temperature, Color),
+                                                 point2: (Temperature, Color),
+                                                 coordinate: Temperature): Color = {
     val newRed = linearFract[Temperature](
       (point1._1, point1._2.red),
       (point2._1, point2._2.red),
@@ -26,4 +34,6 @@ class ColorInterpolationService(linearInterpolationService: InterpolationService
 
     Color(newRed, newGreen, newBlue)
   }
+
+  private val treeMap: TreeMap[Temperature, Color] = TreeMap(points.toSeq: _*)
 }
